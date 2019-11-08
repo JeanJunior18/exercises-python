515287b0cf268f48cf606ae334d96207e436e814
JeanJunior18 <54405723+JeanJunior18@users.noreply.github.com>
2019-11-08 20:20:56 -0300

    Add files via upload

---
+++ createArchivesTXT.py
@@ -0,0 +1,9 @@
+fileName = input("Insert the file name: ")
+fileName = '../' + fileName + '.txt'
+arq1 = open(fileName,"w")
+text = input("Write the text: ")
+arq1.write(text)
+arq1.close()
+read = open(fileName,"r")
+print(read.read())
+read.close()
