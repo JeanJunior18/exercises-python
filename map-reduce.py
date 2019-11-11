dba160f7b5a421e9acd5a5ecf0d7e94cf7ad2d45
JeanJunior18 <54405723+JeanJunior18@users.noreply.github.com>
2019-11-11 16:44:31 -0300

    Add files via upload

---
+++ map-reduce.py
@@ -0,0 +1,29 @@
+# Função Map recebe uma função e uma sequência e opera a sequẽcia na função. Ex:
+
+def fahrenheit(T):
+    return ((float(9)/5)*T+32)
+
+temperaturas = [0,22.5,40,100]
+
+temp = map(fahrenheit, temperaturas)
+temp = list(temp)
+print(temp)
+
+# Pode ser usado para somar listas. Ex:
+a = [1,2,3,4,5]
+b = [5,4,3,2,1]
+
+sum = map(lambda x,y: x+y, a, b) # Este caso usa uma função anônima
+sum = list(sum)
+print(sum)
+
+
+# Função Reduce recebe os mesmos valores que a Map e executa a função 
+from functools import reduce
+def soma(x,y):
+    return x+y
+
+lista = [47,11,42,13]
+
+red = reduce(soma,lista)
+print(red)
